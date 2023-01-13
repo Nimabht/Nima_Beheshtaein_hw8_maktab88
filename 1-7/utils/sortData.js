20e31dd90c9c695b28a85d02c08b7f5956ce7677
nimabht <nimabeheshtaein99@gmail.com>
2023-01-13 11:10:28 +0330

    sortData function fixed.

--- 1-7/utils/sortData.js
+++ 1-7/utils/sortData.js
@@ -12,16 +12,26 @@ function sortData(method) {
   //checks if our method is valid
   if (!dictionary.includes(method)) throw new Error("Invalid sorting method");
   //sorting
-  userData.sort((user1, user2) => {
-    if (
-      String(user1[method]).toLowerCase() < String(user2[method]).toLowerCase()
-    )
-      return 1;
-    if (
-      String(user1[method]).toLowerCase() > String(user2[method]).toLowerCase()
-    )
-      return -1;
-    return 0;
-  });
+  if (method === "uid") {
+    userData.sort((user1, user2) => {
+      if (user1[method] < user2[method]) return 1;
+      if (user1[method] > user2[method]) return -1;
+      return 0;
+    });
+  } else {
+    userData.sort((user1, user2) => {
+      if (
+        String(user1[method]).toLowerCase() <
+        String(user2[method]).toLowerCase()
+      )
+        return 1;
+      if (
+        String(user1[method]).toLowerCase() >
+        String(user2[method]).toLowerCase()
+      )
+        return -1;
+      return 0;
+    });
+  }
   renderTable(userData);
 }
